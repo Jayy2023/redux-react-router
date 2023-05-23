d6d2c41312e485446da8a5dce01334beef63dc95
Jayy2023 <119895456+Jayy2023@users.noreply.github.com>
2023-05-23 17:02:41 -0400

    toggle selected breeds

--- react-ui/src/views/breeds.jsx
+++ react-ui/src/views/breeds.jsx
@@ -1,25 +1,38 @@
+/* eslint-disable no-unused-vars */
 // eslint-disable-next-line no-unused-vars
-import React from 'react';
+import React, {useState} from 'react';
 import { useSelector } from 'react-redux';
 import { selectBreeds } from '../features/breedsSlice';
 
 
 const Breeds = () => {
+  const [selectedBreeds, setSelectedBreeds] = useState([]);
   const data = useSelector(selectBreeds);
   const handleClick = e => {
-    console.log(e.target.breed);
-  }
+    // eslint-disable-next-line no-unused-vars
+    const value = e.target.value.breed;
+    let _selectedBreeds = [];
+    if (selectedBreeds.indexOf(value) !== -1) {
+      const _selectedBreeds = selectedBreeds.filter(x => x !== value);
+      setSelectedBreeds(_selectedBreeds);
+  }else{
+     _selectedBreeds = [...selectedBreeds, value];
+  } 
+  setSelectedBreeds(_selectedBreeds);
+}
 
   const breeds = data && data.map(breed => {
     return <li data-breed={breed} key={breed} onClick={handleClick}>{breed}</li>;
   })
     
-
+  
 
   return (
     <div>
+    {JSON.stringify(selectBreeds)}
     <h1>Breeds</h1>
     <ul>{breeds}</ul>
+
     </div>
   )
 };
