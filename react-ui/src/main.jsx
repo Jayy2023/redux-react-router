aa34e77d9a6a97edf36abe080d422a9a2a072c64
Jayy2023 <119895456+Jayy2023@users.noreply.github.com>
2023-05-23 16:29:32 -0400

    call dog ceo api and handle response

--- react-ui/src/main.jsx
+++ react-ui/src/main.jsx
@@ -3,11 +3,13 @@ import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import store from './app/store';
 import { BrowserRouter } from 'react-router-dom';
+import { fetchBreeds } from './features/breedsSlice';
 
 import App from './App';
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
+store.dispatch(fetchBreeds());
 
 root.render(
   <StrictMode>
